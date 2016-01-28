976c46408e57726e68dccd0ac51a41e221451e8e
Andrzej Pragacz <apragacz@o2.pl>
2016-01-28 23:07:16 +0100

    moved test settings to separate file

--- runtests.py
+++ runtests.py
@@ -1,49 +1,14 @@
 #!/usr/bin/env python
 import sys
-import os.path
 
+from tests import default_settings
 from django.conf import settings
 from django.core.management import execute_from_command_line
 
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-
 def configure_settings():
-    settings.configure(
-        DATABASES={
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': os.path.join(BASE_DIR, 'test_db.sqlite3'),
-            }
-        },
-        INSTALLED_APPS=(
-            'django.contrib.admin',
-            'django.contrib.auth',
-            'django.contrib.contenttypes',
-            'django.contrib.sessions',
-            'django.contrib.messages',
-            'django.contrib.staticfiles',
-
-            'rest_framework',
-            'rest_registration',
-        ),
-        TEMPLATES=(
-            {
-                'BACKEND': 'django.template.backends.django.DjangoTemplates',
-                'DIRS': [],
-                'APP_DIRS': True,
-                'OPTIONS': {
-                    'context_processors': [
-                        'django.template.context_processors.debug',
-                        'django.template.context_processors.request',
-                        'django.contrib.auth.context_processors.auth',
-                        'django.contrib.messages.context_processors.messages',
-                    ],
-                },
-            },
-        )
-    )
+    kwargs = vars(default_settings)
+    settings.configure(**kwargs)
 
 
 def main(args):
---
+++ tests/default_settings.py
@@ -0,0 +1,41 @@
+import os.path
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'test_db.sqlite3'),
+    }
+}
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_registration',
+)
+
+
+TEMPLATES = (
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+)
