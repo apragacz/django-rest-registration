bc517371819227980d7c4db7790f52ccc0d0b592
Andrzej Pragacz <apragacz@o2.pl>
2016-01-30 01:09:16 +0100

    fixed serializer_class_getter and added tests

--- rest_registration/decorators.py
+++ rest_registration/decorators.py
@@ -3,13 +3,16 @@ import types
 
 def serializer_class_getter(class_getter):
 
+    def _get_serializer_class(self):
+        return class_getter()
+
     def decorator(func):
         if not hasattr(func, 'cls'):
             raise Exception('@serializer_class_getter can only decorate'
                             ' @api_view decorated functions')
-        apiview = func.cls
-        apiview.get_serializer_class = types.MethodType(
-            class_getter,
-            apiview)
+        apiview_cls = func.cls
+        apiview_cls.get_serializer_class = types.MethodType(
+            _get_serializer_class,
+            apiview_cls)
         return func
     return decorator
