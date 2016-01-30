cd6721688aceb53bb04d358b046431563fabfbd0
Andrzej Pragacz <apragacz@o2.pl>
2016-01-30 02:30:30 +0100

    test reset_password with expiration

--- tests/api/test_reset_password.py
+++ tests/api/test_reset_password.py
@@ -1,3 +1,6 @@
+import time
+from unittest.mock import patch
+
 from django.test.utils import override_settings
 from rest_framework import status
 
@@ -67,6 +70,24 @@ class ResetPasswordViewTestCase(BaseResetPasswordViewTestCase):
         data['password'] = new_password
         request = self.factory.post('', data)
         response = reset_password(request)
+        self.assert_invalid_response(response, status.HTTP_400_BAD_REQUEST)
+        user.refresh_from_db()
+        self.assertTrue(user.check_password(old_password))
+
+    def test_reset_expired(self):
+        timestamp = int(time.time())
+        old_password = 'password1'
+        new_password = 'password2'
+        user = self.create_test_user(password=old_password)
+        with patch('time.time',
+                   side_effect=lambda: timestamp):
+            signer = ResetPasswordSigner({'user_id': user.pk})
+            data = signer.get_signed_data()
+        data['password'] = new_password
+        request = self.factory.post('', data)
+        with patch('time.time',
+                   side_effect=lambda: timestamp + 3600 * 24 * 8):
+            response = reset_password(request)
         self.assert_invalid_response(response, status.HTTP_400_BAD_REQUEST)
         user.refresh_from_db()
         self.assertTrue(user.check_password(old_password))
