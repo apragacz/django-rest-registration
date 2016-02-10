6fe2576eee82e95da096dd06248c888e8332b5c0
Andrzej Pragacz <apragacz@o2.pl>
2016-02-11 00:45:35 +0100

    added reset password urls

--- rest_registration/api/urls.py
+++ rest_registration/api/urls.py
@@ -1,17 +1,25 @@
 from django.conf.urls import url
 
 from .views import (change_password, login, logout, profile,
+                    send_reset_password_link, reset_password,
                     register, verify_registration,
                     register_email, verify_email)
 
 
 urlpatterns = [
-    url('login/$', login),
-    url('logout/$', logout),
     url('register/$', register),
     url('verify-registration/$', verify_registration),
+
+    url('send-reset-password-link/$', send_reset_password_link),
+    url('reset-password/$', reset_password),
+
+    url('login/$', login),
+    url('logout/$', logout),
+
     url('profile/$', profile),
+
     url('change-password/$', change_password),
+
     url('register-email/$', register_email),
     url('verify-email/$', verify_email),
 ]  # noqa
