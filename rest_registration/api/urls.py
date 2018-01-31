f378a5c1bfa47a0e317ac3ae54ec7332fb739a2f
Andrzej Pragacz <apragacz@o2.pl>
2018-01-31 17:29:23 +0100

    flake8 normalization

--- rest_registration/api/urls.py
+++ rest_registration/api/urls.py
@@ -16,9 +16,11 @@ from .views import (
 app_name = 'rest_registration'
 urlpatterns = [
     url('register/$', register, name='register'),
-    url('verify-registration/$', verify_registration, name='verify-registration'),
+    url('verify-registration/$', verify_registration,
+        name='verify-registration'),
 
-    url('send-reset-password-link/$', send_reset_password_link, name='send-reset-password-link'),
+    url('send-reset-password-link/$', send_reset_password_link,
+        name='send-reset-password-link'),
     url('reset-password/$', reset_password, name='reset-password'),
 
     url('login/$', login, name='login'),
