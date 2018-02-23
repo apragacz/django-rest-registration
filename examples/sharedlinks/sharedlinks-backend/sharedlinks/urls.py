08e4934aebc3be81a7432225fea6775212ca9483
Andrzej Pragacz <apragacz@o2.pl>
2018-02-23 17:33:08 +0100

    Fixed swagger title

--- examples/sharedlinks/sharedlinks-backend/sharedlinks/urls.py
+++ examples/sharedlinks/sharedlinks-backend/sharedlinks/urls.py
@@ -18,7 +18,7 @@ from django.urls import include, path
 
 from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title="LiveOffers API")
+schema_view = get_swagger_view(title="Shared Links API")
 
 api_urlpatterns = [
     path('accounts/', include('rest_registration.api.urls')),
