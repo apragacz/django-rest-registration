5c2965eef3b03987fdef1e5775140a19489efdc7
Andrzej Pragacz <apragacz@o2.pl>
2018-02-27 21:00:32 +0100

    Added codecov integration

--- .travis/run.sh
+++ .travis/run.sh
@@ -10,13 +10,25 @@ print_step() {
     echo "[run.sh] $1"
 }
 
-print_step "Running flake8"
-flake8 .
-print_step "Running isort"
-isort --check
-print_step "Running pylint"
-"${BASE_DIR}/run_pylint.sh"
-print_step "Running unit tests with coverage"
-coverage run "${BASE_DIR}/run_tests.py"
-print_step "Show coverage"
-coverage report
+main() {
+    local codecov_token="${CODECOV_TOKEN:-}"
+    print_step "Running flake8"
+    flake8 .
+    print_step "Running isort"
+    isort --check
+    print_step "Running pylint"
+    "${BASE_DIR}/run_pylint.sh"
+    print_step "Running unit tests with coverage"
+    coverage run "${BASE_DIR}/run_tests.py"
+    if [ -n "${codecov_token}" ]; then
+        print_step "Generating coverage XML"
+        coverage xml
+        print_step "Sending coverage to codecov.io"
+        codecov
+    else
+        print_step "Showing coverage"
+        coverage report
+    fi
+}
+
+main
