8ebe4fe2f8c11c0161251f60d4f70dbeb0170f53
PotatoDrug <zhy.david@gmail.com>
2018-04-04 18:00:20 +0800

    Fixed URL Patterns

--- rest_registration/api/urls.py
+++ rest_registration/api/urls.py
@@ -15,21 +15,21 @@ from .views import (
 
 app_name = 'rest_registration'
 urlpatterns = [
-    url('register/$', register, name='register'),
-    url('verify-registration/$', verify_registration,
+    url('^register/$', register, name='register'),
+    url('^verify-registration/$', verify_registration,
         name='verify-registration'),
 
-    url('send-reset-password-link/$', send_reset_password_link,
+    url('^send-reset-password-link/$', send_reset_password_link,
         name='send-reset-password-link'),
-    url('reset-password/$', reset_password, name='reset-password'),
+    url('^reset-password/$', reset_password, name='reset-password'),
 
-    url('login/$', login, name='login'),
-    url('logout/$', logout, name='logout'),
+    url('^login/$', login, name='login'),
+    url('^logout/$', logout, name='logout'),
 
-    url('profile/$', profile, name='profile'),
+    url('^profile/$', profile, name='profile'),
 
-    url('change-password/$', change_password, name='change-password'),
+    url('^change-password/$', change_password, name='change-password'),
 
-    url('register-email/$', register_email, name='register-email'),
-    url('verify-email/$', verify_email, name='verify-email'),
+    url('^register-email/$', register_email, name='register-email'),
+    url('^verify-email/$', verify_email, name='verify-email'),
 ]
