464667d67c3f52fb2bc9ab144e80532ca784e3b0
Andrzej Pragacz <apragacz@o2.pl>
2018-04-18 21:53:38 +0200

    Refactor: added TestCase, BaseViewTestCase

--- tests/utils.py
+++ tests/utils.py
@@ -1,11 +1,142 @@
-from django.conf import settings
+import contextlib
+from collections import Sequence
 
-from . import default_settings
+from django.contrib.auth import get_user_model
+from django.core import mail
+from django.test import TestCase as DjangoTestCase
+from django.urls import reverse
+from django_dynamic_fixture import G
 
 
-def configure_settings():
-    settings_dict = {
-        key: value for key, value in vars(default_settings).items()
-        if not key.startswith('_') and key.upper() == key
-    }
-    settings.configure(**settings_dict)
+class TestCase(DjangoTestCase):
+
+    def setUp(self):
+        self.user_class = get_user_model()
+
+    def create_test_user(self, **kwargs):
+        password = kwargs.pop('password', None)
+        user = G(self.user_class, **kwargs)
+        if password is not None:
+            user.set_password(password)
+            user.save()
+            user.password_in_plaintext = password
+        return user
+
+    def assert_len_equals(self, collection, expected_len, msg=None):
+        std_msg_format = "{collection} does not have length {expected_len}"
+        std_msg = std_msg_format.format(
+            collection=collection,
+            expected_len=expected_len,
+        )
+        if len(collection) != expected_len:
+            self.fail(self._formatMessage(msg, std_msg))
+
+    def assert_is_between(self, value, start, end, msg=None):
+        std_msg_format = "{value} is not in range [{start}, {end})"
+        std_msg = std_msg_format.format(
+            value=value,
+            start=start,
+            end=end,
+        )
+        if not (start <= value < end):
+            self.fail(self._formatMessage(msg, std_msg))
+
+    def assert_is_not_between(self, value, start, end, msg=None):
+        std_msg_format = "{value} is unexpectedly in range [{start}, {end})"
+        std_msg = std_msg_format.format(
+            value=value,
+            start=start,
+            end=end,
+        )
+        if start <= value < end:
+            self.fail(self._formatMessage(msg, std_msg))
+
+    @contextlib.contextmanager
+    def _assert_mails_sent(self, expected_num):
+        before = len(mail.outbox)
+        sent_emails = EmailMessageContainer()
+        yield sent_emails
+        sent_emails.set(mail.outbox[before:])
+        after = len(mail.outbox)
+        num_of_sent_emails = after - before
+
+        msg_format = "Expected {expected_num} emails to be sent, but found {num_of_sent_emails}"  # noqa: E501
+        msg = msg_format.format(
+            expected_num=expected_num,
+            num_of_sent_emails=num_of_sent_emails,
+        )
+        self.assertEqual(num_of_sent_emails, expected_num, msg=msg)
+
+    @contextlib.contextmanager
+    def assert_mails_sent(self, expected_num):
+        with self._assert_mails_sent(expected_num) as sent_emails:
+            yield sent_emails
+
+    @contextlib.contextmanager
+    def assert_one_mail_sent(self):
+        with self._assert_mails_sent(1) as sent_emails:
+            yield sent_emails
+
+    @contextlib.contextmanager
+    def assert_no_mail_sent(self):
+        with self._assert_mails_sent(0) as sent_emails:
+            yield sent_emails
+
+    def _assert_url_lines_in_text(self, text, expected_num):
+        lines = [line.rstrip() for line in text.split('\n')]
+        url_lines = [
+            line for line in lines
+            if line.startswith('http://') or line.startswith('https://')
+        ]
+        num_of_url_lines = len(url_lines)
+        msg_format = "Found {num_of_url_lines} url lines instead of {expected_num} in:\n{text}"  # noqa: E501
+        msg = msg_format.format(
+            num_of_url_lines=num_of_url_lines,
+            expected_num=expected_num,
+            text=text,
+        )
+        self.assertEqual(num_of_url_lines, expected_num, msg=msg)
+        return url_lines
+
+    def assert_one_url_line_in_text(self, text):
+        url_lines = self._assert_url_lines_in_text(text, 1)
+        return url_lines[0]
+
+
+class BaseViewTestCase(TestCase):
+    APP_NAME = None
+    VIEW_NAME = None
+
+    def setUp(self):
+        super().setUp()
+        self._view_url = None
+
+    @property
+    def full_view_name(self):
+        assert self.APP_NAME
+        assert self.VIEW_NAME
+        return '{self.APP_NAME}:{self.VIEW_NAME}'.format(self=self)
+
+    @property
+    def view_url(self):
+        if self._view_url is None:
+            self._view_url = reverse(self.full_view_name)
+        return self._view_url
+
+
+class EmailMessageContainer(Sequence):
+
+    def __init__(self):
+        self._mails = []
+        self._set = False
+
+    def __len__(self):
+        return len(self._mails)
+
+    def __getitem__(self, i):
+        return self._mails[i]
+
+    def set(self, mails):
+        assert not self._set
+        self._mails = list(mails)
+        self._set = True
