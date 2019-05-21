7323e01c9af62f7c36641ec10512321ecac51545
Oliver Bengs <obengs@flashgrafik.de>
2019-05-21 21:19:06 +0200

    added signals for injecting logic

---
+++ rest_registration/signals.py
@@ -0,0 +1,12 @@
+"""
+Custom signals sent during the registration and activation processes.
+"""
+
+from django.dispatch import Signal
+
+
+# A new user has registered.
+user_registered = Signal(providing_args=["user", "request"])
+
+# A user has activated his or her account.
+user_activated = Signal(providing_args=["user", "request"])
