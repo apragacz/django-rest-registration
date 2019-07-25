c8cb2ecc4aade928e89af947503235c59daee5a7
dustin brown <dbeaubrown@gmail.com>
2019-07-25 10:40:39 -0700

    Add change email verification signal

--- rest_registration/signals.py
+++ rest_registration/signals.py
@@ -9,3 +9,8 @@ user_registered = Signal(providing_args=["user", "request"])
 
 # A user has activated his or her account.
 user_activated = Signal(providing_args=["user", "request"])
+
+# A user has verified his or her new email.
+user_changed_email = Signal(
+    providing_args=["user", "new_email", "old_email", "request"]
+)
