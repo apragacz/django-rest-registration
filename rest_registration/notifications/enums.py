4ba091df39a842754da7ce61cdf480758c3e3927
Andrzej Pragacz <apragacz@o2.pl>
2019-10-06 01:25:09 +0200

    Do not use enum.auto()
    as it is not supported in python 3.4, 3.5

--- rest_registration/notifications/enums.py
+++ rest_registration/notifications/enums.py
@@ -1,11 +1,11 @@
-from enum import Enum, auto
+from enum import Enum
 
 
 class NotificationType(Enum):
-    REGISTER_VERIFICATION = auto()
-    REGISTER_EMAIL_VERIFICATION = auto()
-    RESET_PASSWORD_VERIFICATION = auto()
+    REGISTER_VERIFICATION = 1
+    REGISTER_EMAIL_VERIFICATION = 2
+    RESET_PASSWORD_VERIFICATION = 3
 
 
 class NotificationMethod(Enum):
-    EMAIL = auto()
+    EMAIL = 1
