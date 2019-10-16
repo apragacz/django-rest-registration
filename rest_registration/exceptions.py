3008cc602df8a18916a12e35dd32ded94506faec
Richard Savard <rsavard@d-modules.com>
2019-10-16 12:44:58 -0400

    Add missing translation

--- rest_registration/exceptions.py
+++ rest_registration/exceptions.py
@@ -1,11 +1,15 @@
 from rest_framework.exceptions import APIException
-
+try:
+    from django.utils.translation import ugettext_lazy as _
+except ImportError:
+    def _(text): 
+    	return text
 
 class BadRequest(APIException):
     status_code = 400
-    default_detail = 'Bad Request'
+    default_detail = _('Bad Request')
 
 
 class UserNotFound(APIException):
     status_code = 404
-    default_detail = 'User not found'
+    default_detail = _('User not found')
