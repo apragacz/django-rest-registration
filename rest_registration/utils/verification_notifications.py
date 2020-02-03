a34f176f132b2d0d7a50f455c0e987f452f0d802
Andrzej Pragacz <apragacz@o2.pl>
2020-02-03 02:54:29 +0100

    Refactor code into `send_register_verification_email_notification`
    In regard to issue #100
    
    Changes:
    * Create `send_register_verification_email_notification`
      in `rest_registration.utils.verification_notifications` module
    * Move `RegisterSigner` to `rest_registration.signers.register` module

---
+++ rest_registration/utils/verification_notifications.py
@@ -0,0 +1,21 @@
+
+from rest_registration.notifications.email import (
+    send_verification_notification
+)
+from rest_registration.notifications.enums import NotificationType
+from rest_registration.settings import registration_settings
+from rest_registration.signers.register import RegisterSigner
+from rest_registration.utils.users import get_user_verification_id
+
+
+def send_register_verification_email_notification(request, user):
+    signer = RegisterSigner({
+        'user_id': get_user_verification_id(user),
+    }, request=request)
+    template_config_data = registration_settings.REGISTER_VERIFICATION_EMAIL_TEMPLATES  # noqa: E501
+    notification_data = {
+        'params_signer': signer,
+    }
+    send_verification_notification(
+        NotificationType.REGISTER_VERIFICATION, user,
+        notification_data, template_config_data)
