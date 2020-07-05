308c3fb59110686748b280e01d5d39b5ae8e0558
Andrzej Pragacz <apragacz@o2.pl>
2020-07-06 00:58:32 +0200

    Add tests for authenticate_by_login_data failure

--- tests/utils/test_users.py
+++ tests/utils/test_users.py
@@ -1,9 +1,12 @@
 import pytest
 
+from rest_registration.exceptions import UserNotFound
 from rest_registration.utils.users import (
+    authenticate_by_login_data,
     get_user_field_names,
     get_user_public_field_names
 )
+from tests.helpers.constants import USER_PASSWORD, USERNAME
 from tests.helpers.settings import override_rest_registration_settings
 
 
@@ -176,3 +179,30 @@ def test_get_user_public_field_names(
         kwargs, restreg_settings_override, expected_fields):
     with override_rest_registration_settings(restreg_settings_override):
         assert set(get_user_public_field_names(**kwargs)) == expected_fields
+
+
+@pytest.mark.parametrize('data', [
+    pytest.param(
+        {
+            'login': USERNAME,
+        },
+        id='missing password',
+    ),
+    pytest.param(
+        {
+            'login': USERNAME,
+            'password': USER_PASSWORD + 'blah',
+        },
+        id='invalid password',
+    ),
+    pytest.param(
+        {
+            'username': USERNAME,
+            'password': USER_PASSWORD + 'blah',
+        },
+        id='username but invalid password',
+    ),
+])
+def test_authenticate_by_login_data_fails(user, data):
+    with pytest.raises(UserNotFound):
+        authenticate_by_login_data(data)
