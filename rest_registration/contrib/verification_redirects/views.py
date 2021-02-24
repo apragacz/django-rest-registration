bdb49601664bb2100ab5cb88cc1e1bbfa47a7543
Andrzej Pragacz <apragacz@o2.pl>
2021-02-24 15:10:17 +0100

    Make rest_registration.contrib a non-namespace package
    to avoid issues with MyPy

--- rest_registration/contrib/verification_redirects/views.py
+++ rest_registration/contrib/verification_redirects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect
 from django.views.decorators.http import require_http_methods
+from rest_framework.exceptions import APIException
 
 from rest_registration.api.views.register import process_verify_registration_data
 from rest_registration.api.views.register_email import process_verify_email_data
@@ -46,5 +47,5 @@ def _generic_redirect_view(
     try:
         request = data_processor(data)
         return redirect(success_url)
-    except Exception:
+    except APIException:
         return redirect(failure_url)
