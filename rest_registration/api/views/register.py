9febc3839077c3389742ced2846caa68b2cb36a9
Andrzej Pragacz <apragacz@o2.pl>
2022-10-02 23:52:43 +0200

    Rewrite register, verify_registration as APIView class (#55)

--- rest_registration/api/views/register.py
+++ rest_registration/api/views/register.py
@@ -1,16 +1,16 @@
+from typing import Type
+
 from django.db import transaction
 from django.http import Http404
 from django.utils.translation import gettext as _
 from rest_framework import serializers, status
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.serializers import Serializer
 
 from rest_registration import signals
+from rest_registration.api.views.base import BaseAPIView
 from rest_registration.api.views.login import perform_login
-from rest_registration.decorators import (
-    api_view_serializer_class,
-    api_view_serializer_class_getter
-)
 from rest_registration.exceptions import UserWithoutEmailNonverifiable
 from rest_registration.settings import registration_settings
 from rest_registration.signers.register import RegisterSigner
@@ -23,44 +23,48 @@ from rest_registration.utils.users import (
 from rest_registration.utils.verification import verify_signer_or_bad_request
 
 
-@api_view_serializer_class_getter(
-    lambda: registration_settings.REGISTER_SERIALIZER_CLASS)
-@api_view(['POST'])
-@permission_classes(registration_settings.NOT_AUTHENTICATED_PERMISSION_CLASSES)
-def register(request):
-    '''
-    Register new user.
-    '''
-    if not registration_settings.REGISTER_FLOW_ENABLED:
-        raise Http404()
-    serializer_class = registration_settings.REGISTER_SERIALIZER_CLASS
-    serializer = serializer_class(data=request.data, context={'request': request})
-    serializer.is_valid(raise_exception=True)
+class RegisterView(BaseAPIView):
+    permission_classes = registration_settings.NOT_AUTHENTICATED_PERMISSION_CLASSES
 
-    kwargs = {}
+    def post(self, request: Request) -> Response:
+        '''
+        Register new user.
+        '''
+        if not registration_settings.REGISTER_FLOW_ENABLED:
+            raise Http404()
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
-    if registration_settings.REGISTER_VERIFICATION_ENABLED:
-        verification_flag_field = get_user_setting('VERIFICATION_FLAG_FIELD')
-        kwargs[verification_flag_field] = False
-        email_field_name = get_user_email_field_name()
-        if (email_field_name not in serializer.validated_data
-                or not serializer.validated_data[email_field_name]):
-            raise UserWithoutEmailNonverifiable()
+        kwargs = {}
 
-    with transaction.atomic():
-        user = serializer.save(**kwargs)
         if registration_settings.REGISTER_VERIFICATION_ENABLED:
-            email_sender = registration_settings.REGISTER_VERIFICATION_EMAIL_SENDER
-            email_sender(request, user)
+            verification_flag_field = get_user_setting('VERIFICATION_FLAG_FIELD')
+            kwargs[verification_flag_field] = False
+            email_field_name = get_user_email_field_name()
+            if (email_field_name not in serializer.validated_data
+                    or not serializer.validated_data[email_field_name]):
+                raise UserWithoutEmailNonverifiable()
 
-    signals.user_registered.send(sender=None, user=user, request=request)
-    output_serializer_class = registration_settings.REGISTER_OUTPUT_SERIALIZER_CLASS
-    output_serializer = output_serializer_class(
-        instance=user,
-        context={'request': request},
-    )
-    user_data = output_serializer.data
-    return Response(user_data, status=status.HTTP_201_CREATED)
+        with transaction.atomic():
+            user = serializer.save(**kwargs)
+            if registration_settings.REGISTER_VERIFICATION_ENABLED:
+                email_sender = registration_settings.REGISTER_VERIFICATION_EMAIL_SENDER
+                email_sender(request, user)
+
+        signals.user_registered.send(sender=None, user=user, request=request)
+        output_serializer_class = registration_settings.REGISTER_OUTPUT_SERIALIZER_CLASS
+        output_serializer = output_serializer_class(
+            instance=user,
+            context={'request': request},
+        )
+        user_data = output_serializer.data
+        return Response(user_data, status=status.HTTP_201_CREATED)
+
+    def get_serializer_class(self) -> Type[Serializer]:
+        return registration_settings.REGISTER_SERIALIZER_CLASS
+
+
+register = RegisterView.as_view()
 
 
 class VerifyRegistrationSerializer(serializers.Serializer):  # noqa: E501 pylint: disable=abstract-method
@@ -69,20 +73,25 @@ class VerifyRegistrationSerializer(serializers.Serializer):  # noqa: E501 pylint
     signature = serializers.CharField(required=True)
 
 
-@api_view_serializer_class(VerifyRegistrationSerializer)
-@api_view(['POST'])
-@permission_classes(registration_settings.NOT_AUTHENTICATED_PERMISSION_CLASSES)
-def verify_registration(request):
-    """
-    Verify registration via signature.
-    """
-    user = process_verify_registration_data(
-        request.data, serializer_context={'request': request})
-    signals.user_activated.send(sender=None, user=user, request=request)
-    extra_data = None
-    if registration_settings.REGISTER_VERIFICATION_AUTO_LOGIN:
-        extra_data = perform_login(request, user)
-    return get_ok_response(_("User verified successfully"), extra_data=extra_data)
+class VerifyRegistrationView(BaseAPIView):
+    permission_classes = registration_settings.NOT_AUTHENTICATED_PERMISSION_CLASSES
+
+    def post(self, request: Request) -> Response:
+        """
+        Verify registration via signature.
+        """
+        user = process_verify_registration_data(
+            request.data,
+            serializer_context=self.get_serializer_context(),
+        )
+        signals.user_activated.send(sender=None, user=user, request=request)
+        extra_data = None
+        if registration_settings.REGISTER_VERIFICATION_AUTO_LOGIN:
+            extra_data = perform_login(request, user)
+        return get_ok_response(_("User verified successfully"), extra_data=extra_data)
+
+
+verify_registration = VerifyRegistrationView.as_view()
 
 
 def process_verify_registration_data(input_data, serializer_context=None):
