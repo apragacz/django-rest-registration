87665aa0f49087897ad2a5cc05e23ad04eeea916
Andrzej Pragacz <apragacz@o2.pl>
2023-04-20 00:47:18 +0200

    Do not preserve tests in the package

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-from setuptools import find_packages, setup
+from setuptools import setup
 
-# see setup.cfg for package configuration
-setup(
-    packages=find_packages(exclude=['tests.*', 'tests']),
-)
+# see pyproject.toml for package configuration
+setup()
